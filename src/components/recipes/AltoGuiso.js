d050bbf04df606f1fe7ce8439f4afb23c9631de1
Damucho <damis96@hotmail.com.ar>
2024-08-20 17:53:04 -0300

    feat(text): completed recipes

--- src/components/recipes/AltoGuiso.js
+++ src/components/recipes/AltoGuiso.js
@@ -11,11 +11,12 @@ function AltoGuiso(){
             </div>
 
             <main className="recipe">
-                <h3 className="title">Alto Guiso</h3>
+                <h1 className="title">Alto Guiso</h1>
                 <div className="ingredients">
                     <h5>Ingredientes:</h5>
                     <ul>
                         <li>Corte de carne económico: aprox. 600grs (puede ser de vaca o de cerdo)</li>
+						<li>Fideos, Arroz o legumbres: aprox. 250grs</li>
                         <li>Puré de tomate: 1 lata o 1 caja</li>
                         <li>Cebolla: 1</li>
                         <li>Zanahoria: 1</li>
@@ -25,10 +26,28 @@ function AltoGuiso(){
                 </div>
                 <div className="description">
                     <p>Voy a empezar contando una historia que cambió mi vida: el 7 de agosto del 2011 fue la fecha 1 del 
-                    Torneo Apertura del fútbol argentino y Lanús le ganó 1-0 a San Lorenzo en el Nuevo Gasómetro. Lo más
-					importante no fue la victoria sino que fue un hincha que dijo una frase que quedó para el recuerdo:</p>
+                    Torneo Apertura del fútbol argentino y Lanús le ganó 1-0 a San Lorenzo de visitante en el Nuevo Gasómetro. Lo más
+					importante no fue la victoria sino una frase de un hincha del "granate" que quedó para el recuerdo:</p>
 					
-					<i>15 pesos sale un paty acá, con 15 pesos...con 15 pesos me hago alto guiso</i>
+					<p><i><b>15 pesos sale un paty acá, con 15 pesos...con 15 pesos me hago alto guiso</b></i></p>
+					
+					<p>Para la mayoría de la gente es insólito que una frase tan simple como esa signifique tanto. Resulta que
+					en la época 15 pesos argentinos para un paty de cancha era mucho dinero y para un alto guiso no estabas muy
+					lejos en dinero. Fue tanto lo que generó que se hicieron memes, temas musicales y hasta se creó el "Indice Alto Guiso"
+					para medir la inflación en relación al precio del guiso.</p>
+					
+					<p>Incluso encontraron al señor Alto Guiso muchos años después de la histórica frase. Una historia bien argentina
+					sobre la inflación.</p>
+					
+					<p>Ahora vamos a la receta. Realmente no hay mucho para explicar, el primer paso es cortar la carne en cubitos y
+					sellarla en una cacerola grande, mientras tanto cortar las verduras. Cuando la carne esté sellada reservar.</p>
+					
+					<p>Sin lavar la olla agregar las verduras y saltearlas con sal, luego volver a agregar la carne junto con el
+					puré de tomate y un poco de agua o caldo hasta cubrir. Dejar cocinar por aproximadamente 40 minutos hasta que
+					el líquido se reduzca. Servir muy caliente apenas apagado el fuego.</p>
+					
+					<p>Esta es una de las recetas más personalizables que existen. Se pueden agregar o reemplazar ingredientes
+					como gusten: se puede agregar vino, legumbres, chorizo colorado, pollo...el cielo es el límite.</p>
                 </div>
             </main>
 
--- src/components/recipes/GuisoMoñito.js
+++ src/components/recipes/GuisoMoñito.js
@@ -9,9 +9,37 @@ function GuisoMoñito(){
                 <img src="img/guiso de fideos moñito.jpg" width="100%" height="20%" alt="loading..." />
             </div>
 
-            <div>
-                <p>Guiso de Fideos Moñito</p>
-            </div>
+            <main className="recipe">
+                <h3 className="title">Guiso de Fideos Moñito</h3>
+                <div className="ingredients">
+                    <h5>Ingredientes:</h5>
+                    <ul>
+                        <li>Fideos Moñito: medio paquete</li>
+						<li>Corte de carne económico: aprox. 500grs (puede ser de vaca o de cerdo)</li>
+                        <li>Puré de tomate: 1 lata o 1 caja</li>
+                        <li>Cebolla: 1</li>
+                        <li>Zanahoria: 1</li>
+                        <li>Ajo: 2 dientes</li>
+                        <li>Sal, Orégano, Pimentón y Ají Molido: a gusto</li>
+                    </ul>
+                </div>
+                <div className="description">
+                    <p>Esta receta es un homenaje a un hincha de Talleres que dijo que haría un guiso de fideos moñito en una
+					cena romántica con su señora. No dudo ni un solo segundo en responder a la pregunta.</p>
+					
+					<p>Esto es algo económico y muy sencillo ideal para el invierno. Primero vamos a picar la carne y la vamos
+					a saltear vuelta y vuelta con aceite en una cacerola grande. Al mismo tiempo picamos la cebolla, zanahoria y ajo.
+					Cuando la carne esté sellada la reservamos y salteamos las verduras con sal hasta que estén blandas.</p>
+					
+					<p>Volvemos a colocar la carne y luego incorporamos el puré de tomate y un poco de agua o caldo hasta cubrir.
+					Dejamos cocinar por 40 minutos aproximadamente y al final agregamos los fideos moñito, mezclamos todo hasta que
+					los fideos estén un poco menos cocidos que al dente y apagamos el fuego. Servir bien caliente.</p>
+					
+					<p>Esta es una de las recetas más personalizables que existen. Se pueden agregar o reemplazar ingredientes
+					como gusten: se puede agregar vino, legumbres, chorizo colorado, pollo...el cielo es el límite.</p>
+					
+                </div>
+            </main>
 
             <Footer />
         </>
