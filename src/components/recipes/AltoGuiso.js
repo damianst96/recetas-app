7d8b36b80afdb3a3af657278451ea03a6fcf2519
Damucho <damis96@hotmail.com.ar>
2024-09-30 00:40:13 -0300

    finished 'Alto Guiso'

--- src/components/recipes/AltoGuiso.js
+++ src/components/recipes/AltoGuiso.js
@@ -1,14 +1,24 @@
+import { useEffect, useState } from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
 import '../../styles.css';
 
 function AltoGuiso(){
-    return(
+    const [recipe, setRecipe] = useState(null);
+
+    useEffect(() => {
+        fetch('/api/alto-guiso')
+            .then((response) => response.json())
+            .then((data) => setRecipe(data))
+            .catch((error) => console.error('Error fetching data:', error));
+    }, []);
+
+    return (
         <>
             <Header />
 
             <main className="recipe">
-            <h1 className="title">Alto Guiso</h1>
+            <h1 className="title">{recipe ? recipe.title : 'Loading...'}</h1>
 
             <div className="banner-main">
                 <img src="img/alto guiso.jpg" className="banner-image" width="100%" alt="loading..." />
@@ -18,43 +28,30 @@ function AltoGuiso(){
                 <div className="ingredients">
                     <h5>Ingredientes:</h5>
                     <ul>
-                        <li>Corte de carne económico: aprox. 600grs (puede ser de vaca o de cerdo)</li>
-						<li>Fideos, Arroz o legumbres: aprox. 250grs</li>
-                        <li>Puré de tomate: 1 lata o 1 caja</li>
-                        <li>Cebolla: 1</li>
-                        <li>Zanahoria: 1</li>
-                        <li>Ajo: 2 dientes</li>
-                        <li>Sal, Orégano, Pimentón y Ají Molido: a gusto</li>
+                        {recipe ? recipe.ingredients.map(function(i) {
+                            return <li>{i}</li>
+                        }) : "Loading..."}
                     </ul>
                 </div>
-                <div className="description">
-                    <p>Voy a empezar contando una historia que cambió mi vida: el 7 de agosto del 2011 fue la fecha 1 del 
-                    Torneo Apertura del fútbol argentino y Lanús le ganó 1-0 a San Lorenzo de visitante en el Nuevo Gasómetro. Lo más
-					importante no fue la victoria sino una frase de un hincha del "granate" que quedó para el recuerdo:</p>
-					
-					<p><i><b>15 pesos sale un paty acá, con 15 pesos...con 15 pesos me hago alto guiso</b></i></p>
 
-                    <div className="banner">
-                        <p><img src="img/alto-guiso.jpg" width="100%" height="20%" alt="loading..." /></p>
-                    </div>
-					
-					<p>Para la mayoría de la gente es insólito que una frase tan simple como esa signifique tanto. Resulta que
-					en la época 15 pesos argentinos para un paty de cancha era mucho dinero y para un alto guiso no estabas muy
-					lejos en dinero. Fue tanto lo que generó que se hicieron memes, temas musicales y hasta se creó el "Indice Alto Guiso"
-					para medir la inflación en relación al precio del guiso.</p>
-					
-					<p>Incluso encontraron al señor Alto Guiso muchos años después de la histórica frase. Una historia bien argentina
-					sobre la inflación.</p>
-					
-					<p>Ahora vamos a la receta. Realmente no hay mucho para explicar, el primer paso es cortar la carne en cubitos y
-					sellarla en una cacerola grande, mientras tanto cortar las verduras. Cuando la carne esté sellada reservar.</p>
-					
-					<p>Sin lavar la olla agregar las verduras y saltearlas con sal, luego volver a agregar la carne junto con el
-					puré de tomate y un poco de agua o caldo hasta cubrir. Dejar cocinar por aproximadamente 40 minutos hasta que
-					el líquido se reduzca. Servir muy caliente apenas apagado el fuego.</p>
-					
-					<p>Esta es una de las recetas más personalizables que existen. Se pueden agregar o reemplazar ingredientes
-					como gusten: se puede agregar vino, legumbres, chorizo colorado, pollo...el cielo es el límite.</p>
+                <div className="description">
+                    
+                    {recipe ? recipe.description.map(function(num, index) {
+                        if (index === 1){
+                            return <div>
+
+                                <p><i><b>{num}</b></i></p>
+
+                                <div className="banner">
+                                    <p><img src="img/alto-guiso.jpg" width="100%" height="20%" alt="loading..." /></p>
+                                </div>
+
+                            </div>
+                        } else {
+                            return <p>{num}</p>
+                        }
+                    }) : "Loading..."}
+                    
                 </div>
             </main>
 
--- src/server/server.js
+++ src/server/server.js
@@ -15,9 +15,9 @@ app.set("view engine", "ejs");
 const publicPath = path.join(__dirname, '/public');
 app.use(express.static(publicPath));
 
-app.get('/api/data', (req, res) => {
-    res.json({ message: 'Hello from Express!' });
-});
+// app.get('/api/data', (req, res) => {
+//     res.json({ message: 'Hello from Express!' });
+// });
 
 app.get('/api/guiso-monito', (req, res) => {
     res.json({ 
@@ -36,6 +36,31 @@ app.get('/api/guiso-monito', (req, res) => {
         description: ["Esta receta es un homenaje a un hincha de Talleres que dijo que haría un guiso de fideos moñito en una cena romántica con su señora. No dudo ni un solo segundo en responder a la pregunta.",
             "Esto es algo económico y muy sencillo ideal para el invierno. Primero vamos a picar la carne y la vamos a saltear vuelta y vuelta con aceite en una cacerola grande. Al mismo tiempo picamos la cebolla, zanahoria y ajo. Cuando la carne esté sellada la reservamos y salteamos las verduras con sal hasta que estén blandas.",
             "Volvemos a colocar la carne y luego incorporamos el puré de tomate y un poco de agua o caldo hasta cubrir.	Dejamos cocinar por 40 minutos aproximadamente y al final agregamos los fideos moñito, mezclamos todo hasta que	los fideos estén un poco menos cocidos que al dente y apagamos el fuego. Servir bien caliente.",
+            "Esta es una de las recetas más personalizables que existen. Se pueden agregar o reemplazar ingredientes como gusten: se puede agregar vino, legumbres, chorizo colorado, pollo...el cielo es el límite."
+        ]
+    });
+});
+
+app.get('/api/alto-guiso', (req, res) => {
+    res.json({ 
+        id: 2,
+        title: 'Alto Guiso',
+        image: "img/alto guiso.jpg",
+        ingredients: [
+            "Corte de carne económico: aprox. 600grs (puede ser de vaca o de cerdo)",
+            "Fideos, Arroz o legumbres: aprox. 250grs",
+            "Puré de tomate: 1 lata o 1 caja",
+            "Cebolla: 1",
+            "Zanahoria: 1",
+            "Ajo: 2 dientes",
+            "Sal, Orégano, Pimentón y Ají Molido: a gusto"
+        ],
+        description: ["Voy a empezar contando una historia que cambió mi vida: el 7 de agosto del 2011 fue la fecha 1 del Torneo Apertura del fútbol argentino y Lanús le ganó 1-0 a San Lorenzo de visitante en el Nuevo Gasómetro. Lo más	importante no fue la victoria sino una frase de un hincha del 'granate' que quedó para el recuerdo:",
+            "15 pesos sale un paty acá, con 15 pesos...con 15 pesos me hago alto guiso",
+            "Para la mayoría de la gente es insólito que una frase tan simple como esa signifique tanto. Resulta que en la época 15 pesos argentinos para un paty de cancha era mucho dinero y para un alto guiso no estabas muy lejos en dinero. Fue tanto lo que generó que se hicieron memes, temas musicales y hasta se creó el 'Indice Alto Guiso'	para medir la inflación en relación al precio del guiso.",
+            "Incluso encontraron al señor Alto Guiso muchos años después de la histórica frase. Una historia bien argentina	sobre la inflación.",
+            "Ahora vamos a la receta. Realmente no hay mucho para explicar, el primer paso es cortar la carne en cubitos y sellarla en una cacerola grande, mientras tanto cortar las verduras. Cuando la carne esté sellada reservar.",
+            "Sin lavar la olla agregar las verduras y saltearlas con sal, luego volver a agregar la carne junto con el puré de tomate y un poco de agua o caldo hasta cubrir. Dejar cocinar por aproximadamente 40 minutos hasta que el líquido se reduzca. Servir muy caliente apenas apagado el fuego.",
             "Esta es una de las recetas más personalizables que existen. Se pueden agregar o reemplazar ingredientes como gusten: se puede agregar vino, legumbres, chorizo colorado, pollo...el cielo es el límite."
         ]
     });
